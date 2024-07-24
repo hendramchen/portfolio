3f4f8d7556d9eb8cf3732dbe3a62c0d10d197fd9
Hendra Putu <mchen.putu@gmail.com>
2024-07-24 18:46:03 +0800

    add type for h1 and remove pagination

--- components/h1.tsx
+++ components/h1.tsx
@@ -1,3 +1,3 @@
-export default function H1({ children }) {
+export default function H1({ children }: { children: React.ReactNode }) {
   return <h1 className="mb-8 text-2xl not-prose">{children}</h1>;
 }
